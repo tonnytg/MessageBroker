593d37d0979e26a09444447105de8cb7f33d0591
Antonio Thomacelli <tonnytg@gmail.com>
2021-10-29 23:17:42 -0300

    feat: add new segregation

--- main.go
+++ main.go
@@ -1,49 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/streadway/amqp"
 	"messagebroker/pkg/rabbit"
-	"os"
 	"time"
 )
 
 func main() {
-	// I used https://api.cloudamqp.com in free tier for this example
-	user := os.Getenv("RABBITMQ_USER")
-	password := os.Getenv("RABBITMQ_PASSWORD")
-	host := os.Getenv("RABBITMQ_HOST")
-	port := os.Getenv("RABBITMQ_PORT")
-	vhost := os.Getenv("RABBITMQ_VHOST")
-	database := os.Getenv("RABBITMQ_DATABASE")
 
-	conn, err := rabbit.GetConn(fmt.Sprintf("amqp://%s:%s@%s/%s", user, password, host, port, vhost, database))
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println("Reading message...")
+	message := flag.String("message", "", "Message to send")
+	flag.Parse()
+	fmt.Println(*message)
+	time.Sleep(time.Second * 1)
 
-	go func() {
-		for {
-			time.Sleep(time.Second)
-			conn.Publish("test-key", []byte(`{"message":"test1"}`))
-		}
-	}()
-
-	err = conn.StartConsumer("test-queue", "test-key", handler, 2)
-
-	if err != nil {
-		panic(err)
-	}
-
-	forever := make(chan bool)
-	<-forever
-}
-
-func handler(d amqp.Delivery) bool {
-	if d.Body == nil {
-		fmt.Println("Error, no message body!")
-		return false
-	}
-	fmt.Println(string(d.Body))
-	return true
+	rabbit.InitRabbitMQ()
 }
